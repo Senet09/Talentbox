b3764b0543f4828155d059780cd8397b065e41b3
Senet <utkarshsinghco@outlook.com>
2023-09-15 03:37:17 +0530

    api change

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,7 +8,7 @@ import { setToken } from "../slices/user";
 import { Navigate, Link, useNavigate } from "react-router-dom";
 import "../Signup.css";
 
-const Backend_Url = "http://ec2-13-200-110-133.ap-south-1.compute.amazonaws.com:8000/v1/adduser";
+const Backend_Url = "http://ec2-13-200-110-133.ap-south-1.compute.amazonaws.com:8000/api/v1/adduser";
 
 function SignUp() {
   const navi = useNavigate();
